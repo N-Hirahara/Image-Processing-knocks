2e7a1ea68119a15004aead40526450eab16e8e00
n-hirahara <hira.08.p@gmail.com>
2019-05-22 12:33:18 +0900

    tutorial

---
+++ tutorial/sample.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+
+
+# In[ ]:
+
+
+img = cv2.imread('../Gasyori100knock-master/assets/imori.jpg')
+cv2.imshow("imori", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
